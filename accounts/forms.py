1231d886ab79277c54c5a5395b0d2975491410ec
permin0ff <perminoff-ilya@yandex.ru>
2023-06-02 22:07:46 +0300

    reg 1

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=200)
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput())
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2',)
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,32 @@
-from .forms import SignUpForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+    initial = {'key': 'value'}
+    template_name = 'registration/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='/')
+
+        return render(request, self.template_name, {'form': form})
+
+
 
