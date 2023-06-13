5ccbfc6b06e789df6e7af6863d30f29a6545f0c9
permin0ff <perminoff-ilya@yandex.ru>
2023-06-13 10:44:12 +0300

    bootstrap5

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,13 +4,20 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Profile
 from django_summernote.widgets import SummernoteWidget
 
+
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput())
-    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput())
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter First Name'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter Last Name'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter Username'}))
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Enter your E-Mail'}))
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Enter password'}))
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Confirm Password'}))
 
     class Meta:
         model = User
@@ -20,10 +27,11 @@ class SignUpForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=100,
                                required=True,
-                               widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+                               widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
     password = forms.CharField(max_length=50,
                                required=True,
-                               widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+                               widget=forms.PasswordInput(
+                                   attrs={"class": "form-control mb-1", 'placeholder': 'Password'}))
     remember_me = forms.BooleanField(required=False)
 
     class Meta:
@@ -34,9 +42,9 @@ class LoginForm(AuthenticationForm):
 class UpdateUserForm(forms.ModelForm):
     username = forms.CharField(max_length=100,
                                required=True,
-                               widget=forms.TextInput())
+                               widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
     email = forms.EmailField(required=True,
-                             widget=forms.TextInput())
+                             widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
 
     class Meta:
         model = User
@@ -44,8 +52,8 @@ class UpdateUserForm(forms.ModelForm):
 
 
 class UpdateProfileForm(forms.ModelForm):
-    avatar = forms.ImageField(widget=forms.FileInput())
-    bio = forms.CharField(widget=SummernoteWidget())
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={"class": "form-control mb-1", 'placeholder': 'Username'}))
+    bio = forms.CharField(widget=SummernoteWidget(attrs={"class": "form-control", 'placeholder': 'Enter search term...', 'summernote': {'width': '100%', 'height': '300px'}}))
 
     class Meta:
         model = Profile
