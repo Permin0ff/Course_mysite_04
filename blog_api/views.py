b237a01df364a0a3d2042890c12560738042e2fc
permin0ff <perminoff-ilya@yandex.ru>
2023-06-18 15:39:53 +0300

    bootstrap5

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,13 +1,27 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+
 from blog.models import Post
 from .serializers import PostSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['author']
+    search_fields = ['body', 'author__profile__bio']
+    ordering_fields = ['author_id', 'publish']
+    ordering = ['body']
+    pagination_class = StandardResultsSetPagination
 
 
 ''' def get_queryset(self):
--- mysite/settings.py
+++ mysite/settings.py
@@ -174,5 +174,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']  # new
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',  # new
+    'PAGE_SIZE': 5 ,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+
 }
